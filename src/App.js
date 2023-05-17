fe4957e22aaae5308ee539f9572296f0e907019e
yunchan312 <phenomenal312@hufs.ac.kr>
2023-05-17 16:18:56 +0900

    styled component study

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+
+const ratationAnimation = keyframes`
+0% {transform:rotate(0deg);
+border-radius:0px;} 50% {border-radius:100px;} 100%{transform:rotate(360deg);
+  border-radius:0px;}`;
+
+const Box = styled.div`
+  height: 100px;
+  width: 100px;
+  background-color: tomato;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: ${(props) => props.theme.border};
+  animation: ${ratationAnimation} 1s linear infinite;
+  ${Emoji} {
+    margin-top: auto;
+    margin-bottom: auto;
+    &:hover {
+      font-size: 60px;
+      cursor: pointer;
+    }
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Title>Hello</Title>
+      <Emoji as="p">🙂</Emoji>
+      <Box>
+        <Emoji as="p">🙂</Emoji>
+      </Box>
+    </Wrapper>
   );
 }
 
